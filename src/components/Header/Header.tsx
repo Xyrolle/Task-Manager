84ea53e70b7b6c98d9648ab2ceedf325834fa002
Eugene_Ostapenco <evgheni@mediapark.com>
2020-07-14 17:44:49 +0300

    modify main header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 
 import './Header.css';
+import Vector from '../../assets/Vector.svg';
+import SearchIcon from '../../assets/SearchIcon.svg';
+import Plus from '../../assets/Plus.svg';
 
 const Header: React.FC = () => {
   return (
     <header className='mainHeader'>
       <nav className='nav'>
-        <ul>
-          <li>Home</li>
-          <li>Projects</li>
-          <li>Planning</li>
-          <li>Everything</li>
-          <li>Calendar</li>
-          <li>People</li>
-        </ul>
+        <div className='mainHeadList'>
+          <div className='logo'>
+            <span>Task</span> <span>Manager</span>
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Planning</li>
+            <li>Everything</li>
+            <li>Calendar</li>
+            <li>People</li>
+          </ul>
+        </div>
+        <div className='mainHeadBtns'>
+          <button type='button' className='addTeamBtn'>
+            <img src='//cdn-pjs.teamwork.com/tko/public/assets/svg/inlinehelp/inviteusers.svg' />{' '}
+            Add your team
+          </button>
+          <button type='button' className='upgradeBtn'>
+            Upgrade Now
+          </button>
+          <span className='searchCircle'>
+            <img src={SearchIcon} alt='search' className='search' />
+          </span>
+          <span className='profileCircle'>
+            <span>EO</span>
+          </span>
+          <img src={Vector} alt='vector' className='vector' />
+        </div>
       </nav>
     </header>
   );
