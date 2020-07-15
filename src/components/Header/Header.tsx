04c0447c51bd44cdc8e1e3610d11050770e2d368
Eugene_Ostapenco <evgheni@mediapark.com>
2020-07-15 15:25:05 +0300

    modify main header add styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './Header.css';
 import Vector from '../../assets/Vector.svg';
 import SearchIcon from '../../assets/SearchIcon.svg';
+import Bell from '../../assets/Bell.svg';
 import Plus from '../../assets/Plus.svg';
 
 const Header: React.FC = () => {
@@ -25,13 +26,18 @@ const Header: React.FC = () => {
         <div className='mainHeadBtns'>
           <button type='button' className='addTeamBtn'>
             <img src='//cdn-pjs.teamwork.com/tko/public/assets/svg/inlinehelp/inviteusers.svg' />{' '}
-            Add your team
+            Add Your Team
           </button>
           <button type='button' className='upgradeBtn'>
             Upgrade Now
           </button>
           <span className='searchCircle'>
             <img src={SearchIcon} alt='search' className='search' />
+          </span>
+          <div className='plus'></div>
+          <div className='folder'></div>
+          <span className='bellContainer'>
+            <img src={Bell} alt='bell' className='bell' />
           </span>
           <span className='profileCircle'>
             <span>EO</span>
