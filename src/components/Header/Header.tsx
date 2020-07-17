72b84eb9d5b435a3d4566373d97929e98ad75da4
Eugene_Ostapenco <evgheni@mediapark.com>
2020-07-17 13:05:45 +0300

    Fixes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,48 +7,47 @@ import Bell from '../../assets/Bell.svg';
 import Logo from '../../assets/Logo.png';
 
 const Header: React.FC = () => {
-	return (
-		<header className='mainHeader'>
-			<nav className='nav'>
-				<div className='mainHeadList'>
-					<div className='logo'>
-						<span>Task</span> 
-            <span>Manager</span>
-					</div>
-					<ul>
-						<li>Home</li>
-						<li>Projects</li>
-						<li>Planning</li>
-						<li>Everything</li>
-						<li>Calendar</li>
-						<li>People</li>
-					</ul>
-				</div>
-				<div className='mainHeadBtns'>
-					<button type='button' className='addTeamBtn'>
-						<img
-							src='//cdn-pjs.teamwork.com/tko/public/assets/svg/inlinehelp/inviteusers.svg'
-					  	alt='add team'
-						/>
-						Add Your Team
-					</button>
-					<button type='button' className='upgradeBtn'>
-						Upgrade Now
-					</button>
-					<span className='searchCircle'>
-						<img src={SearchIcon} alt='search' className='search' />
-					</span>
-					<div className='plus' />
-					<div className='folder' />
-					<span className='bellContainer'>
-						<img src={Bell} alt='bell' className='bell' />
-					</span>
-					<span className='profileCircle'>EO</span>
-					<img src={Vector} alt='vector' className='vector' />
-				</div>
-			</nav>
-		</header>
-	);
+  return (
+    <header className='mainHeader'>
+      <nav className='nav'>
+        <div className='mainHeadList'>
+          <div className='logo'>
+            <img src={Logo} alt='logo' className='logo' />
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Planning</li>
+            <li>Everything</li>
+            <li>Calendar</li>
+            <li>People</li>
+          </ul>
+        </div>
+        <div className='mainHeadBtns'>
+          <button type='button' className='addTeamBtn'>
+            <img
+              src='//cdn-pjs.teamwork.com/tko/public/assets/svg/inlinehelp/inviteusers.svg'
+              alt='add team'
+            />
+            Add Your Team
+          </button>
+          <button type='button' className='upgradeBtn'>
+            Upgrade Now
+          </button>
+          <span className='searchCircle'>
+            <img src={SearchIcon} alt='search' className='search' />
+          </span>
+          <div className='plus' />
+          <div className='folder' />
+          <span className='bellContainer'>
+            <img src={Bell} alt='bell' className='bell' />
+          </span>
+          <span className='profileCircle'>EO</span>
+          <img src={Vector} alt='vector' className='vector' />
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
