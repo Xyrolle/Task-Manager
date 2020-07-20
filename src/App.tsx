55774e204a247dd56dc765c29142a1c36e7b2ba3
Eugene_Ostapenco <evgheni@mediapark.com>
2020-07-20 11:44:37 +0300

    Fix router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import AllPages from './pages/AllPages';
 import { AppProvider } from './context/AppContext';
@@ -7,9 +8,11 @@ import './App.css';
 
 function App() {
   return (
-    <AppProvider>
-      <AllPages />
-    </AppProvider>
+    <Router>
+      <AppProvider>
+        <AllPages />
+      </AppProvider>
+    </Router>
   );
 }
 export default App;
