26cc5a44235a9e8b6f6748384a3f513b9ed758ec
Eugene_Ostapenco <evgheni@mediapark.com>
2020-07-22 13:42:20 +0300

    Fix a few bugs

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route } from 'react-router-dom';
 import ContentHeader from './ContentHeader/ContentHeader';
 import TaskLists from '../../components/MainContent/Tasks/TaskLists/TaskLists';
 import { AgendaContent } from '../../components/MainContent/Agenda/AgendaContent';
-import AgendaCreate from '../../components/MainContent/Agenda/AgendaCreate'
+import AgendaCreate from '../../components/MainContent/Agenda/AgendaCreate';
 import { AgendaDetails } from '../../components/MainContent/Agenda/AgendaDetails';
 
 import './MainContent.css';
 
 const MainContent: React.FC = () => {
-	return (
-		<div>
-			<ContentHeader />
-			<div className='contentContainer'>
-				<Route exact path='/agenda' component={AgendaContent} />
-				<Route exact path='/agenda/create' component={AgendaCreate} />
-				<Route path='/agenda/:agendaID' component={AgendaDetails} />
-				<Route path='/tasks/:projectID' component={Tasks} />
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <ContentHeader />
+      <div className='contentContainer'>
+        <Route exact path='/agenda' component={AgendaContent} />
+        <Route exact path='/agenda/create' component={AgendaCreate} />
+        <Route path='/agenda/:agendaID' component={AgendaDetails} />
+        <Route path='/tasks/:projectID' component={TaskLists} />
+      </div>
+    </div>
+  );
 };
 
 export default MainContent;
