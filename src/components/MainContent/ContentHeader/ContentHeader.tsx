443109ee9d0a1951a77c825e9cdd86b7f1cd6610
Serghey <serii.hmeli123123@mail.ru>
2020-07-23 11:54:24 +0300

    responsiveness, add modal for adding task lists

--- src/components/MainContent/ContentHeader/ContentHeader.tsx
+++ src/components/MainContent/ContentHeader/ContentHeader.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './ContentHeader.css';
+import { AppContext } from '../../../context/AppContext';
 
 const ContentHeader: React.FC = () => {
+	const ctx = useContext(AppContext);
+
+	if (!ctx) {
+		throw new Error('You probably forgot to put <AppProvider>.');
+	}
+
 	return (
 		<div className='contentHeader'>
 			<h1 className='contentHeader-left'>Tasks</h1>
 			<div className='contentHeader-right'>
-				<button className='btn'>Add Task List</button>
+				<button className='btn' onClick={() => ctx.openTaskListModal()}>
+					Add Task List
+				</button>
 			</div>
 		</div>
 	);
--- src/pages/AllPages.tsx
+++ src/pages/AllPages.tsx
@@ -1,26 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, Fragment } from 'react';
 import ProjectsPage from './ProjectsPage/ProjectsPage';
 import Modal from '../components/Modal/Modal';
+import AddTaskListModal from '../components/MainContent/Tasks/AddTaskListModal/AddTaskListModal';
 import { AppContext } from '../context/AppContext';
 
 const AllPages: React.FC = () => {
-  const ctx = useContext(AppContext);
+	const ctx = useContext(AppContext);
 
-  if (!ctx) {
-    throw new Error('You probably forgot to put <AppProvider>.');
-  }
-  return (
-    <div>
-      {ctx.modalVisible ? (
-        <>
-          <Modal />
-          <ProjectsPage />
-        </>
-      ) : (
-          <ProjectsPage />
-        )}
-    </div>
-  );
+	if (!ctx) {
+		throw new Error('You probably forgot to put <AppProvider>.');
+	}
+	return (
+		<div>
+			{
+				ctx.addTaskListModal ? <Fragment>
+					<AddTaskListModal />
+					<ProjectsPage />
+				</Fragment> :
+				ctx.modalVisible ? <Fragment>
+					<Modal />
+					<ProjectsPage />
+				</Fragment> :
+				<ProjectsPage />}
+		</div>
+	);
 };
 
 export default AllPages;
