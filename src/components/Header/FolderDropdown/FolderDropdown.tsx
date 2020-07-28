e493217cea884a6bfcd2245d8c4f29e95f688901
Eugene_Ostapenco <evgheni@mediapark.com>
2020-07-28 08:26:04 +0300

    add dropdowns and search bar to header

---
+++ src/components/Header/FolderDropdown/FolderDropdown.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './FolderDropdown.css';
+
+const FolderDropdown: React.FC = () => {
+  return (
+    <div className='folderDropdownContainer'>
+      <header className='folderDropdownHeader'>
+        <div>Latest | All</div>{' '}
+        <input type='search' autoComplete='off' placeholder='Search' />
+      </header>
+      <div className='folderDropdownContent' role='contentinfo'></div>
+    </div>
+  );
+};
+
+export default FolderDropdown;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,24 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom'
+import React, { useContext, useState, Fragment } from 'react';
+
 import './Header.css';
 import Vector from '../../assets/Vector.svg';
 import SearchIcon from '../../assets/SearchIcon.svg';
+import CancelSearch from '../../assets/CancelSearch.svg';
 import Bell from '../../assets/Bell.svg';
 import Logo from '../../assets/Logo.png';
 import PlusDropdown from './PlusDropdown/PlusDropdown';
+import FolderDropdown from './FolderDropdown/FolderDropdown';
+import BellDropdown from './BellDropdown/BellDropdown';
 import { AppContext } from '../../context/AppContext';
 
-
 const Header: React.FC = () => {
-	const [plusDropdownIsOpen, setPlusDropdownIsOpen] = useState<boolean>(false);
-	const ctx = useContext(AppContext);
+  const [plusDropdownIsOpen, setPlusDropdownIsOpen] = useState<boolean>(false);
+  const [folderDropdownIsOpen, setFolderDropdownIsOpen] = useState<boolean>(
+    false
+  );
+  const [bellDropdownIsOpen, setBellDropdownIsOpen] = useState<boolean>(false);
+  const [searchInputIsOpen, setSearchInputIsOpen] = useState<boolean>(false);
+  const ctx = useContext(AppContext);
 
   if (!ctx) {
     throw new Error('You probably forgot to put <AppProvider>.');
@@ -45,30 +52,78 @@ const Header: React.FC = () => {
           </div>
         </div>
         <div className='mainHeadBtns'>
-          <button type='button' className='addTeamBtn' onClick={ctx.openModal}>
-            <img
-              src='//cdn-pjs.teamwork.com/tko/public/assets/svg/inlinehelp/inviteusers.svg'
-              alt='add team'
-            />
-            <span>Add Your Team</span>
-          </button>
-          <button type='button' className='upgradeBtn'>
-            Upgrade Now
-          </button>
-          <span className='searchCircle'>
-            <img src={SearchIcon} alt='search' className='search' />
-          </span>
+          {!searchInputIsOpen ? (
+            <Fragment>
+              <button
+                type='button'
+                className='addTeamBtn'
+                onClick={ctx.openModal}
+              >
+                <img
+                  src='//cdn-pjs.teamwork.com/tko/public/assets/svg/inlinehelp/inviteusers.svg'
+                  alt='add team'
+                />
+                <span>Add Your Team</span>
+              </button>
+              <button type='button' className='upgradeBtn'>
+                Upgrade Now
+              </button>
+              <span
+                className='searchCircle'
+                onClick={() => setSearchInputIsOpen(true)}
+              >
+                <img src={SearchIcon} alt='search' className='search' />
+              </span>
+            </Fragment>
+          ) : (
+            <div className='searchInputContainer'>
+              <img src={SearchIcon} alt='search' className='searchInInput' />
+              <input
+                id='search'
+                className='searchInput'
+                type='search'
+                autoComplete='off'
+                placeholder='Search'
+              />
+              <img
+                src={CancelSearch}
+                alt='cancel search'
+                className='cancelSearchInInput'
+                onClick={() => setSearchInputIsOpen(false)}
+              />
+            </div>
+          )}
           <div className='plusContainer'>
             <div
               className={!plusDropdownIsOpen ? 'plus' : 'whitePlus'}
-              onClick={() => setPlusDropdownIsOpen(!plusDropdownIsOpen)}
-            ></div>
+              onClick={() => [
+                setPlusDropdownIsOpen(!plusDropdownIsOpen),
+                setFolderDropdownIsOpen(false),
+                setBellDropdownIsOpen(false),
+              ]}
+            />
             {plusDropdownIsOpen && <PlusDropdown />}
           </div>
-          <div className='folder' />
-          <span className='bellContainer'>
+          <div
+            className='folder'
+            onClick={() => [
+              setFolderDropdownIsOpen(!folderDropdownIsOpen),
+              setPlusDropdownIsOpen(false),
+              setBellDropdownIsOpen(false),
+            ]}
+          />
+          {folderDropdownIsOpen && <FolderDropdown />}
+          <span
+            className='bellContainer'
+            onClick={() => [
+              setBellDropdownIsOpen(!bellDropdownIsOpen),
+              setPlusDropdownIsOpen(false),
+              setFolderDropdownIsOpen(false),
+            ]}
+          >
             <img src={Bell} alt='bell' className='bell' />
           </span>
+          {bellDropdownIsOpen && <BellDropdown />}
           <span className='profileCircle'>EO</span>
           <img src={Vector} alt='vector' className='vector' />
         </div>
