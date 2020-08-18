d1979988f69282cf70072fafc8ba9ee3fe1ba68f
Your Name <you@example.com>
2020-08-18 17:15:48 +0300

    fixed login  and add team

--- src/context/queries.tsx
+++ src/context/queries.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { axiosConfig } from 'utils/axiosConfig';
 
 export const getUserInfo = async () => {
+  axiosConfig.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
   try {
     const response = await axios.get(
       `http://46.101.172.171:8008/users/by_token/`,
